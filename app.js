2fd411c8b6681b9fc046dbdbd735ad22f97bb175
adarsh darokar <adarshdarokar55@gmail.com>
2025-07-16 23:07:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function rps(user, computer) {
+  if (user === computer) return "draw";
+  if (user === "rock" && computer === "scissor") return "user";
+  if (user === "scissor" && computer === "paper") return "user";
+  if (user === "paper" && computer === "rock") return "user";
+  return "computer";
+}
+console.log(rps("scissor", "rock"));
+console.log(rps("paper", "rock"));
+console.log(rps("rock", "scissor"));
+console.log(rps("rock", "paper"));
