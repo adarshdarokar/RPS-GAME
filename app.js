060c16ac01eef460f08ff724415e0248950f5390
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-07-17 19:02:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ function rps(user, computer) {
   if (user === "paper" && computer === "rock") return "user";
   return "computer";
 }
+
+
+
 console.log(rps("scissor", "rock"));
 console.log(rps("paper", "rock"));
 console.log(rps("rock", "scissor"));
 console.log(rps("rock", "paper"));
+console.log(rps("scissor", "paper"));
